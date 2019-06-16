4052db56e3903ddc1ba35019af72dc034a86c64d
Logan McAnsh <logan@mcan.sh>
2019-06-16 14:59:48 -0400

    feat: configure MusicKit in _app and store in state
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App, { Container, AppContext } from 'next/app';
+import * as Sentry from '@sentry/browser';
+
 import Layout from '~/components/layout';
+import Head from 'next/head';
+
+Sentry.init({
+  dsn: process.env.SENTRY,
+  environment: process.env.NODE_ENV,
+  release: `guac@${process.env.VERSION}`,
+});
+
+declare global {
+  interface Window {
+    MusicKit: any;
+  }
+}
 
 class MyApp extends App {
+  state = { MusicKit: null };
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    Sentry.withScope(scope => {
+      scope.setExtras(errorInfo);
+      Sentry.captureException(error);
+    });
+
+    super.componentDidCatch(error, errorInfo);
+  }
+
+  componentDidMount = () => {
+    window.MusicKit.configure({
+      developerToken: this.props.pageProps.developerToken,
+      app: {
+        name: 'Miniature Guacamole 🥑',
+        build: process.env.VERSION,
+        icon: 'http://localhost:4000/static/default.jpeg',
+      },
+    });
+
+    this.setState({ MusicKit: window.MusicKit });
+  };
+
+  static getInitialProps = async ({ Component, ctx }: AppContext) => {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  };
+
   render() {
     const { Component, pageProps } = this.props;
+    const { MusicKit } = this.state;
 
     return (
       <Container>
+        <Head>
+          <script src="https://js-cdn.music.apple.com/musickit/v1/musickit.js" />
+        </Head>
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} MusicKit={MusicKit} />
         </Layout>
       </Container>
     );
