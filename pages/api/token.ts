e5789e38681944cc6bf7430fc253dbc150bec23b
Logan McAnsh <logan@mcan.sh>
2019-12-16 00:33:24 -0500

    feat: add token expiration date to response
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/api/token.ts
+++ pages/api/token.ts
@@ -1,5 +1,6 @@
-import jwt from 'jsonwebtoken';
-import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import { addDays } from "date-fns";
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   const privateKey = `-----BEGIN PRIVATE KEY-----
@@ -8,16 +9,19 @@ ${String(process.env.GUAC_AUTH_KEY)}
 
   const teamId = process.env.GUAC_TEAM_ID;
   const keyId = process.env.GUAC_KEY_ID;
-  const jwtToken = await jwt.sign({}, privateKey, {
-    algorithm: 'ES256',
-    expiresIn: '180d',
+  const token = await jwt.sign({}, privateKey, {
+    algorithm: "ES256",
+    expiresIn: "180d",
     issuer: teamId,
     header: {
-      alg: 'ES256',
-      kid: keyId,
-    },
+      alg: "ES256",
+      kid: keyId
+    }
   });
 
+  const now = new Date();
+  const expirationDate = addDays(now, 180).getTime();
+
   res.status(200);
-  res.json({ token: jwtToken });
+  res.json({ token, expirationDate });
 };
