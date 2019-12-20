7b73a1af7156cb903354379e98e8ac94daeda2e1
Logan McAnsh <logan@mcan.sh>
2019-12-20 15:44:43 -0500

    feat: add album duration
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/album/[id].tsx
+++ pages/album/[id].tsx
@@ -3,13 +3,37 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { SimpleImg } from 'react-simple-img';
+import { ellipsis } from 'polished';
 
 import { fetchMusic, artworkForMediaItem } from '~/utils';
-import { AlbumResponse } from '~/@types/MusicKit/album';
+import { AlbumResponse, TracksDatum } from '~/@types/MusicKit/album';
 import { DEFAULT_ALBUM_ART } from '~/config';
 
 const generateSrcSet = (artworkURL: string | undefined, sizes: number[]) =>
   sizes.map(size => `${artworkForMediaItem(artworkURL, size)} ${size}w`).join();
+
+function msToTime(duration: number) {
+  const minutes = Math.floor((duration / (1000 * 60)) % 60);
+  const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  if (hours && minutes) {
+    const hourText = hours === 1 ? 'hour' : 'hours';
+    const minuteText = hours === 1 ? 'minute' : 'minutes';
+    return `${hours} ${hourText}, ${minutes} ${minuteText}`;
+  }
+
+  const minuteText = hours === 1 ? 'hour' : 'hours';
+  return `${minutes} ${minuteText}`;
+}
+
+const getAlbumDuration = (tracks: TracksDatum[]) => {
+  const durationInMilliseconds = tracks.reduce((acc, cur) => {
+    const duration = cur?.attributes?.durationInMillis ?? 0;
+    return acc + duration;
+  }, 0);
+
+  return msToTime(durationInMilliseconds);
+};
 
 const AlbumPage: NextPage<{
   devToken: string;
@@ -47,68 +71,117 @@ const AlbumPage: NextPage<{
           }
         `}
       >
-        <SimpleImg
-          height={200}
-          width={200}
-          placeholder={DEFAULT_ALBUM_ART}
-          src={artworkForMediaItem(album.attributes?.artwork?.url, 400)}
-          alt={album.attributes?.name}
-          srcSet={srcSet}
-          style={{ width: 200, height: 200, borderRadius: 4 }}
-        />
-        <div>
+        <div
+          css={`
+            height: 20rem;
+            width: 20rem;
+            border-radius: 0.4rem;
+
+            @media (max-width: 500px) {
+              width: 12rem;
+              height: 12rem;
+            }
+          `}
+        >
+          <SimpleImg
+            height={200}
+            width={200}
+            placeholder={DEFAULT_ALBUM_ART}
+            src={artworkForMediaItem(album.attributes?.artwork?.url, 400)}
+            alt={album.attributes?.name}
+            srcSet={srcSet}
+            applyAspectRatio
+          />
+        </div>
+        <div
+          css={`
+            h1 {
+              font-size: 1.6rem;
+              font-weight: 500;
+            }
+          `}
+        >
           <h1>{album.attributes?.name}</h1>
           <h2>{album.attributes?.artistName}</h2>
         </div>
       </div>
-      <ol
+      <div
         css={`
-          font-size: 1.6rem;
-          list-style: none;
-          counter-reset: my-awesome-counter;
           padding: 0 2rem;
-
-          li {
-            display: grid;
-            grid-template-columns: auto 1fr;
-            align-items: center;
-            border-top: 1px solid rgba(0, 0, 0, 0.37);
-            border-bottom: 1px solid rgba(0, 0, 0, 0.37);
-            padding: 0 1rem;
-          }
-
-          li::before {
-            counter-increment: my-awesome-counter;
-            content: counter(my-awesome-counter);
-            color: rgba(0, 0, 0, 0.37);
-            padding-right: 1.5rem;
-          }
+          margin-bottom: 2rem;
         `}
       >
-        {album.relationships?.tracks?.data?.map(track => (
-          <li key={track.id}>
-            <button
-              onClick={async () => {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-                // @ts-ignore
-                const music = window.MusicKit.getInstance();
-                await music.setQueue({ song: track.id });
-                await music.play();
-              }}
-              type="button"
+        <ol
+          css={`
+            font-size: 1.6rem;
+            list-style: none;
+            padding: 0;
+            margin-bottom: 2rem;
+
+            li {
+              display: grid;
+              grid-template-columns: auto 1fr;
+              align-items: center;
+              border-top: 1px solid rgba(0, 0, 0, 0.37);
+              padding: 0 1rem;
+              :last-of-type {
+                border-bottom: 1px solid rgba(0, 0, 0, 0.37);
+              }
+            }
+          `}
+        >
+          {album.relationships?.tracks?.data?.map(track => (
+            <li
+              key={track.id}
               css={`
-                width: 100%;
-                background: none;
-                border: none;
-                padding: 1.2rem 0;
-                text-align: left;
+                ::before {
+                  content: "${track.attributes?.trackNumber}";
+                  color: rgba(0, 0, 0, 0.37);
+                  padding-right: 1.5rem;
+                }
               `}
             >
-              <span>{track.attributes?.name}</span>
-            </button>
-          </li>
-        ))}
-      </ol>
+              <button
+                onClick={async () => {
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                  // @ts-ignore
+                  const music = window.MusicKit.getInstance();
+                  await music.setQueue({ song: track.id });
+                  await music.play();
+                }}
+                type="button"
+                css={`
+                  width: 100%;
+                  background: none;
+                  border: none;
+                  padding: 1.2rem 0;
+                  text-align: left;
+                  ${ellipsis('100%')}
+                `}
+              >
+                <span>{track.attributes?.name}</span>
+              </button>
+            </li>
+          ))}
+        </ol>
+
+        {album.relationships?.tracks?.data &&
+          album.relationships?.tracks?.data.length > 0 && (
+            <p
+              css={`
+                font-size: 1.6rem;
+                color: rgba(0, 0, 0, 0.37);
+              `}
+            >
+              {album.attributes?.trackCount} song
+              {album.attributes?.trackCount &&
+              album.attributes?.trackCount === 1
+                ? ''
+                : 's'}
+              , {getAlbumDuration(album.relationships?.tracks?.data)}
+            </p>
+          )}
+      </div>
     </>
   );
 };
@@ -135,7 +208,7 @@ AlbumPage.getInitialProps = async context => {
   }
   // 5. fetch the current user's recently added music
   const initialData = await fetchMusic(
-    `/v1/me/library/albums/${query.id}?include=tracks,genres`,
+    `/v1/me/library/albums/${query.id}?include=tracks`,
     devToken,
     userToken
   );
