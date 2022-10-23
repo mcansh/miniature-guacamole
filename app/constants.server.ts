618363098e880c81fc9115efc3330094b202d11a
Logan McAnsh <logan@mcan.sh>
2022-10-23 16:19:44 -0400

    chore: use env variable for private key
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/constants.server.ts
+++ app/constants.server.ts
@@ -1,3 +1,7 @@
+if (!process.env.GUAC_AUTH_KEY) {
+  throw new Error("GUAC_AUTH_KEY is required");
+}
+
 if (!process.env.GUAC_TEAM_ID) {
   throw new Error(`GUAC_TEAM_ID is required`);
 }
@@ -14,6 +18,7 @@ if (!process.env.MUSIC_API) {
   throw new Error(`MUSIC_API is required`);
 }
 
+export const GUAC_AUTH_KEY = process.env.GUAC_AUTH_KEY;
 export const GUAC_TEAM_ID = process.env.GUAC_TEAM_ID;
 export const GUAC_KEY_ID = process.env.GUAC_KEY_ID;
 export const SESSION_PASSWORD = process.env.SESSION_PASSWORD;
--- app/gen-developer-token.server.ts
+++ app/gen-developer-token.server.ts
@@ -1,27 +1,17 @@
-import fs from "node:fs";
-import path from "node:path";
 import jwt from "jsonwebtoken";
-import { GUAC_KEY_ID, GUAC_TEAM_ID } from "./constants.server";
+
+import { GUAC_KEY_ID, GUAC_TEAM_ID, GUAC_AUTH_KEY } from "./constants.server";
 
 export function generateDeveloperToken(): string {
-  let privateKey = fs.readFileSync(
-    path.join(process.cwd(), "AuthKey_V69Q5367TF.p8")
-  );
+  let start = `-----BEGIN PRIVATE KEY-----\n`;
+  let end = `\n-----END PRIVATE KEY-----`;
+  let privateKey = start + GUAC_AUTH_KEY + end;
 
-  const now = Math.floor(Date.now() / 1000);
+  let now = Math.floor(Date.now() / 1000);
 
   return jwt.sign(
-    {
-      iss: GUAC_TEAM_ID,
-      exp: now + 15777000,
-      iat: now,
-    },
+    { iss: GUAC_TEAM_ID, exp: now + 15777000, iat: now },
     privateKey,
-    {
-      header: {
-        alg: "ES256",
-        kid: GUAC_KEY_ID,
-      },
-    }
+    { header: { alg: "ES256", kid: GUAC_KEY_ID } }
   );
 }
